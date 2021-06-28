766d409e51a2dc19f106b22ca7e30bbeab70c304
dweiss <dweiss@ourveryown.com.au>
2021-06-28 16:50:58 +1000

    move RetrofitClient to separate file

--- app/src/main/java/com/manangatangy/musifesti/model/Repository.kt
+++ app/src/main/java/com/manangatangy/musifesti/model/Repository.kt
@@ -1,17 +1,18 @@
 package com.manangatangy.musifesti.model
 
 import android.util.Log
-import com.google.gson.GsonBuilder
-import okhttp3.OkHttpClient
 import retrofit2.HttpException
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.http.GET
-import retrofit2.http.Headers
-import java.util.concurrent.TimeUnit
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
+
+interface ApiClient {
+    suspend fun getFestivals(): List<MusicFestival>
+}
+
+interface BaseUrlProvider {
+    fun getBaseUrl(): String
+}
 
 object Repository: KoinComponent {
     private val apiClient: ApiClient by inject()
@@ -25,54 +26,6 @@ object Repository: KoinComponent {
         } catch (exception: Throwable) {
             ApiResult.Error(exception)
         }
-    }
-}
-
-interface ApiClient {
-    suspend fun getFestivals(): List<MusicFestival>
-}
-
-interface BaseUrlProvider {
-    fun getBaseUrl(): String
-}
-
-private const val CALL_TIMEOUT = 10L
-
-class RetrofitClientImpl(baseUrlProvider: BaseUrlProvider): ApiClient, KoinComponent  {
-
-    private val okHttpClient: OkHttpClient by inject()
-
-    // retrofit >2.6.0 supports coroutines directly
-    // Ref: https://proandroiddev.com/using-retrofit-2-with-kotlin-coroutines-cb112f0fb738
-    override suspend fun getFestivals(): List<MusicFestival> = musicFestivalsService.getFestivals()
-
-    interface MusicFestivalsApi {
-        @Headers("Cache-Control: no-cache")
-        @GET("/codingtest/api/v1/festivals")
-        suspend fun getFestivals(): List<MusicFestival>
-    }
-
-    val musicFestivalsService: MusicFestivalsApi by lazy {
-        Log.d("Repository", "new Retrofit created")
-        Retrofit.Builder()
-            .baseUrl(baseUrlProvider.getBaseUrl())
-            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
-            .client(okHttpClient)
-            .build().create(MusicFestivalsApi::class.java)
-    }
-}
-
-interface OkHttpClientProvider {
-    fun getOkHttpClient(): OkHttpClient
-}
-
-object OkHttpClientProviderImpl: OkHttpClientProvider {
-    override fun getOkHttpClient(): OkHttpClient {
-        Log.d("Repository", "new OkHttpClient created")
-        return OkHttpClient.Builder()
-            .callTimeout(CALL_TIMEOUT, TimeUnit.SECONDS)
-            .cache(null)
-            .build()
     }
 }
 
---
+++ app/src/main/java/com/manangatangy/musifesti/model/RetrofitClient.kt
@@ -0,0 +1,52 @@
+package com.manangatangy.musifesti.model
+
+import android.util.Log
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import org.koin.standalone.KoinComponent
+import org.koin.standalone.inject
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import java.util.concurrent.TimeUnit
+
+private const val CALL_TIMEOUT = 10L
+
+class RetrofitClientImpl(baseUrlProvider: BaseUrlProvider): ApiClient, KoinComponent {
+
+    private val okHttpClient: OkHttpClient by inject()
+
+    // retrofit >2.6.0 supports coroutines directly
+    // Ref: https://proandroiddev.com/using-retrofit-2-with-kotlin-coroutines-cb112f0fb738
+    override suspend fun getFestivals(): List<MusicFestival> = musicFestivalsService.getFestivals()
+
+    interface MusicFestivalsApi {
+        @Headers("Cache-Control: no-cache")
+        @GET("/codingtest/api/v1/festivals")
+        suspend fun getFestivals(): List<MusicFestival>
+    }
+
+    val musicFestivalsService: MusicFestivalsApi by lazy {
+        Log.d("Repository", "new Retrofit created")
+        Retrofit.Builder()
+            .baseUrl(baseUrlProvider.getBaseUrl())
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .client(okHttpClient)
+            .build().create(MusicFestivalsApi::class.java)
+    }
+}
+
+interface OkHttpClientProvider {
+    fun getOkHttpClient(): OkHttpClient
+}
+
+object OkHttpClientProviderImpl: OkHttpClientProvider {
+    override fun getOkHttpClient(): OkHttpClient {
+        Log.d("Repository", "new OkHttpClient created")
+        return OkHttpClient.Builder()
+            .callTimeout(CALL_TIMEOUT, TimeUnit.SECONDS)
+            .cache(null)
+            .build()
+    }
+}
